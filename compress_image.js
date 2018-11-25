fd36202f0aa23f1fd64bd61379856772a09eed97
shingo <reg@shin-go.net>
2018-11-25 23:18:03 +0900

    画像の書き出し処理の追加

--- compress_image.js
+++ compress_image.js
@@ -1,13 +1,25 @@
 const ImageFill = require("scenegraph").ImageFill;
 const Group = require("scenegraph").Group;
 const Artboard = require("scenegraph").Artboard;
+const application = require("application");
+const fs = require("uxp").storage.localFileSystem;
+let count = 0;
 
 async function compressImageCommand(selection,documentRoot) {
+  count += 1;
   console.log('compressImageCommand', selection,documentRoot);
 
   // 画像ノードの取得
   let imageNodes = getImageNodes(documentRoot.children);
 
+  // 画像ファイルの書き出し
+
+  for (let i = 0; i < imageNodes.length; i++){
+    await createReditions(imageNodes[i]);
+  }
+
+
+  // 画像ファイルの読み込み
 }
 
 
@@ -18,12 +30,31 @@ function getImageNodes(nodes){
     if(child.fill instanceof ImageFill){
       imageNodes.push(child);
     }else if(child instanceof Group || child instanceof Artboard){
-      console.log(getImageNodes(child.children));
+      imageNodes.push(...getImageNodes(child.children));
     }
   });
   return imageNodes;
-
 }
+
+async function createReditions(node){
+  const folder = await fs.getTemporaryFolder();
+  const file = await folder.createFile(node.guid + "_" + count + ".jpg");
+  let renditionSettings = [{
+    node: node,
+    outputFile: file,
+    type: application.RenditionType.JPG,
+    quality: 80,
+    scale: 1
+  }];
+  await application.createRenditions(renditionSettings)    // [1]
+    .then(results => {                             // [2]
+        console.log(`JPG rendition has been saved at ${results[0].outputFile.nativePath}`);
+    })
+    .catch(error => {                              // [3]
+        console.log(error);
+    });
+}
+
 
 
 module.exports = compressImageCommand;
